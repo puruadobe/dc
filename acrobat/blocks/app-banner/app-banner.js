3b07f4bebc8022f88e0f85d41d6fce16caed3655
Purushotham Doreswamachary <doreswam@Purushothams-MacBook-Pro.local>
2023-08-30 09:51:08 -0700

    formatting and open button size

--- acrobat/blocks/app-banner/app-banner.js
+++ acrobat/blocks/app-banner/app-banner.js
@@ -1,39 +1,38 @@
-import {createTag} from "../../scripts/miloUtils.js";
+import { createTag } from "../../scripts/miloUtils.js";
 
-export default async function init(el){
-  if(getMobileOperatingSystem() == 'unknown'){
+export default async function init(el) {
+  if (getMobileOperatingSystem() == 'unknown') {
     el.innerHTML = '';
     return;
   }
   const children = el.querySelectorAll(':scope > div');
 
-
   // Get the first source's srcset
   let imagePath = document.querySelector('source').getAttribute('srcset');
   let appStoreLinks = children[6].textContent.trim().split(',');
 
-  
+
   createTag.then((createTag) => {
     const appBannerLeft = createTag('div', { class: 'app-banner-left' }, '');
-    const closeBtn = createTag('div', { class: 'app-banner-close', role: 'text', 'aria-label':'Close banner' }, '×');
+    const closeBtn = createTag('div', { class: 'app-banner-close', role: 'text', 'aria-label': 'Close banner' }, '×');
     appBannerLeft.append(closeBtn);
     const icon = createTag('div', { class: 'app-banner-icon' }, '');
-    const appIcon = createTag('img',{src: `${imagePath}`},'');
+    const appIcon = createTag('img', { src: `${imagePath}` }, '');
     icon.append(appIcon);
     appBannerLeft.append(icon);
     const appComp = createTag('div', { class: 'app-banner-title' }, children[0].textContent.trim());
     const appDesc = createTag('div', { class: 'app-banner-description' }, children[2].textContent.trim());
-    const appStars = createTag('div', { class: 'app-banner-stars', role: 'text', 'aria-label':'Average rating 5 stars' }, '');
-    const star = createTag('span',{}, '★');
-    const star1 = createTag('span',{}, '★');
-    const star2 = createTag('span',{}, '★');
-    const star3 = createTag('span',{}, '★');
+    const appStars = createTag('div', { class: 'app-banner-stars', role: 'text', 'aria-label': 'Average rating 5 stars' }, '');
+    const star = createTag('span', {}, '★');
+    const star1 = createTag('span', {}, '★');
+    const star2 = createTag('span', {}, '★');
+    const star3 = createTag('span', {}, '★');
     appStars.append(star, star1, star2, star3);
     let reviews = children[4].textContent.trim();
     let openButton = children[5].textContent.trim();
-    const appReviews = createTag('div', { class: 'app-banner-reviews', 'aria-label':'' }, reviews);
+    const appReviews = createTag('div', { class: 'app-banner-reviews', 'aria-label': '' }, reviews);
     const appBannerRight = createTag('div', { class: 'app-banner-right' }, '');
-    const open = createTag('div', { class: 'app-banner-button', role: 'text', 'aria-label': openButton}, openButton);
+    const open = createTag('div', { class: 'app-banner-button', role: 'text', 'aria-label': openButton }, openButton);
     appBannerRight.append(open);
     const appBannerContent = createTag('div', { class: 'app-banner-content' }, '');
     const appBanner = createTag('div', { class: 'app-banner' }, '');
